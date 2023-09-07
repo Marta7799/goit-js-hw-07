cc36dc6187e6c47265f17a9ea1df4f7670a2b497
Mart <marta.nikiel@yahoo.com>
2023-09-07 09:25:29 +0200

    added task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const items = [];
+
+galleryItems.forEach((element) => {
+  const galleryItem = document.createElement("li");
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = element.original;
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute("title", element.description);
+  galleryImage.alt = element.description;
+
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+  items.push(galleryItem);
+});
+gallery.append(...items);
+
+let imageWindow = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
